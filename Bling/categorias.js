b1fa8ef177408e3bd40f11a4d3ae0d3e2e3eb1fb
fsilvainacio <fsilvainacio@gmail.com>
2022-04-12 08:32:18 -0300

    start

---
+++ Bling/categorias.js
@@ -0,0 +1,99 @@
+function get_categoriaTrayBling(cat_){
+	for(var i_cat = 0; i_cat < categoriaTrayBling.length; i_cat++){		
+		if (categoriaTrayBling[i_cat][1] == cat_) {
+			return categoriaTrayBling[i_cat][2];
+		}
+	}
+	return '';
+}
+function get_ncmBling(cat_){
+	for(var i_cat = 0; i_cat < categoriaTrayBling.length; i_cat++){		
+		if (categoriaTrayBling[i_cat][2] == cat_) {
+			return categoriaTrayBling[i_cat][0][0];
+		}
+	}
+	return '';
+}
+function get_cestBling(cat_){
+	for(var i_cat = 0; i_cat < categoriaTrayBling.length; i_cat++){		
+		if (categoriaTrayBling[i_cat][2] == cat_) {
+			return categoriaTrayBling[i_cat][0][1];
+		}
+	}
+	return '';
+}
+
+function dados_categorias() {
+	categoriaTrayBling = [
+		[['9405.11.90', '21.123.00'], 'LUMINÁRIAS',	'1 - LUMINÁRIAS'],
+		[['8539.52.00', '09.005.00'], 'LÂMPADAS',	'19 - LÂMPADAS'],
+		[['9405.11.90', '21.123.00'], 'TRILHOS',	'53 - TRILHOS'],
+		[['9405.11.90', '21.123.00'], 'PERFIL LED',	'61 - PERFIL LED'],
+		[['9405.49.00', '21.125.00'], 'FITA LED',	'125 - FITA LED'],
+		[['9405.21.00', '21.124.00'], 'Abajures',	'1 - LUMINÁRIAS>>15 - Abajures'],
+		[['9405.11.90', '21.123.00'], 'Arandelas',	'1 - LUMINÁRIAS>>11 - Arandelas'],
+		[['9405.49.00', '21.125.00'], 'Balizadores',	'1 - LUMINÁRIAS>>79 - Balizadores'],						// Esse mudou cest
+		[['9405.11.90', '21.123.00'], 'Embutidos de Solo',	'1 - LUMINÁRIAS>>13 - Embutidos de Solo'],
+		[['9405.49.00', '21.125.00'], 'Espetos',	'1 - LUMINÁRIAS>>93 - Espetos'],								// Esse mudou cest
+		[['9405.11.90', '21.123.00'], 'Lanternas de Jardim',	'1 - LUMINÁRIAS>>117 - Lanternas de Jardim'],
+		[['9405.11.90', '21.123.00'], 'Luminárias de Embutir',	'1 - LUMINÁRIAS>>9 - Luminárias de Embutir'],
+		[['9405.11.90', '21.123.00'], 'Luminárias de Emergência',	'1 - LUMINÁRIAS>>109 - Luminárias de Emergência'],
+		[['9405.21.00', '21.124.00'], 'Luminárias de Mesa',	'1 - LUMINÁRIAS>>89 - Luminárias de Mesa'],
+		[['9405.11.90', '21.123.00'], 'Luminárias de Piso',	'1 - LUMINÁRIAS>>17 - Luminárias de Piso'],				// Esse mudou cest
+		[['9405.11.90', '21.123.00'], 'Lustres',	'1 - LUMINÁRIAS>>3 - Lustres'],
+		[['9405.11.90', '21.123.00'], 'Pendentes',	'1 - LUMINÁRIAS>>5 - Pendentes'],
+		[['9405.11.90', '21.123.00'], 'Spot de Embutir',	'1 - LUMINÁRIAS>>135 - Spot de Embutir'],
+		[['9405.11.90', '21.123.00'], 'Spot de Sobrepor',	'1 - LUMINÁRIAS>>137 - Spot de Sobrepor'],
+		[['9405.11.90', '21.123.00'], 'Plafons de Sobrepor',	'1 - LUMINÁRIAS>>7 - Plafons de Sobrepor'],
+		[['9405.11.90', '21.123.00'], 'Plafons de Embutir',	'1 - LUMINÁRIAS>>133 - Plafons de Embutir'],
+		[['9405.11.90', '21.123.00'], 'Luminárias de Sobrepor',	'1 - LUMINÁRIAS>>131 - Luminárias de Sobrepor'],
+		[['9405.11.90', '21.123.00'], 'Postes',	'1 - LUMINÁRIAS>>95 - Postes'],
+		[['9405.41.00', '21.125.00'], 'Projetores',	'1 - LUMINÁRIAS>>97 - Projetores'],
+		[['9405.11.90', '21.123.00'], 'Acessórios para Luminária',	'1 - LUMINÁRIAS>>83 - Acessórios para Luminária'],
+		[['8539.52.00', '09.005.00'], 'AR70',	'19 - LÂMPADAS>>29 - AR70'],
+		[['8539.52.00', '09.005.00'], 'AR111',	'19 - LÂMPADAS>>35 - AR111'],
+		[['8539.52.00', '09.005.00'], 'Bulbo',	'19 - LÂMPADAS>>39 - Bulbo'],
+		[['8539.52.00', '09.005.00'], 'Dicróica / PAR16',	'19 - LÂMPADAS>>23 - Dicróica / PAR16'],
+		[['8539.52.00', '09.005.00'], 'Filamento',	'19 - LÂMPADAS>>43 - Filamento'],
+		[['9405.49.00', '21.125.00'], 'Fita LED 12v / 24v',	'125 - FITA LED>>49 - Fita LED 12v / 24v'],				// Esse mudou cest
+		[['9405.49.00', '21.125.00'], 'Fita LED 110v / 220v',	'125 - FITA LED>>51 - Fita LED 110v / 220v'],		// Esse mudou cest
+		[['8504.40.21', '12.001.00'], 'Fontes e Drivers',	'125 - FITA LED>>87 - Fontes e Drivers'],
+		[['8539.52.00', '09.005.00'], 'G9 / G4',	'19 - LÂMPADAS>>45 - G9 / G4'],
+		[['8539.52.00', '09.005.00'], 'Mini Dicróica',	'19 - LÂMPADAS>>21 - Mini Dicróica'],
+		[['8539.52.00', '09.005.00'], 'Palito R7s',	'19 - LÂMPADAS>>41 - Palito R7s'],
+		[['8539.52.00', '09.005.00'], 'PAR20',	'19 - LÂMPADAS>>25 - PAR20'],
+		[['8539.52.00', '09.005.00'], 'PAR30 / PAR38',	'19 - LÂMPADAS>>27 - PAR30 / PAR38'],
+		[['8539.52.00', '09.005.00'], 'Tubular T5/T8',	'19 - LÂMPADAS>>47 - Tubular T5/T8'],
+		[['8539.52.00', '09.005.00'], 'Vela',	'19 - LÂMPADAS>>85 - Vela'],
+		[['8504.40.90', '12.001.00'], 'Dimmer',	'19 - LÂMPADAS>>99 - Dimmer'],
+		[['8536.50.90', '12.004.00'], 'Sensores',	'19 - LÂMPADAS>>115 - Sensores'],
+		[['9405.11.90', '21.123.00'], 'Acessórios para Lâmpada',	'19 - LÂMPADAS>>111 - Acessórios para Lâmpada'],
+		[['9405.11.90', '21.123.00'], 'Acessórios para Fita LED',	'125 - FITA LED>>113 - Acessórios para Fita LED'],
+		[['9405.11.90', '21.123.00'], 'Spots para Trilho',	'53 - TRILHOS>>57 - Spots para Trilho'],
+		[['9405.11.90', '21.123.00'], 'Kit Trilho e Spots',	'53 - TRILHOS>>127 - Kit Trilho e Spots'],
+		[['9405.11.90', '21.123.00'], 'Luminárias para Trilho',	'53 - TRILHOS>>129 - Luminárias para Trilho'],
+		[['9405.11.90', '21.123.00'], 'Trilhos',	'53 - TRILHOS>>55 - Trilhos'],
+		[['9405.11.90', '21.123.00'], 'Acessórios para Trilho e Spot',	'53 - TRILHOS>>59 - Acessórios para Trilho e Spot'],
+		[['9405.11.90', '21.123.00'], 'Perfil de Embutir',	'61 - PERFIL LED>>63 - Perfil de Embutir'],
+		[['9405.11.90', '21.123.00'], 'Perfil de Sobrepor',	'61 - PERFIL LED>>65 - Perfil de Sobrepor'],
+		[['9405.11.90', '21.123.00'], 'Acessórios para Perfil',	'61 - PERFIL LED>>91 - Acessórios para Perfil'],
+	];
+}
+
+/* 
+// Novos: 
+OK 1 - LUMINÁRIAS>>131 - Luminárias de Sobrepor
+OK 1 - LUMINÁRIAS>>7 - Plafons de Sobrepor
+OK 1 - LUMINÁRIAS>>133 - Plafons de Embutir
+OK 1 - LUMINÁRIAS>>135 - Spot de Embutir
+OK 1 - LUMINÁRIAS>>137 - Spot de Sobrepor
+OK 125 - FITA LED
+OK 125 - FITA LED>>49 - Fita LED 12v / 24v
+OK 125 - FITA LED>>51 - Fita LED 110v / 220v
+OK 125 - FITA LED>>87 - Fontes e Drivers
+OK 125 - FITA LED>>113 - Acessórios para Fita LED
+OK 53 - TRILHOS>>57 - Spots para Trilho
+OK 53 - TRILHOS>>127 - Kit Trilho e Spots
+OK 53 - TRILHOS>>129 - Luminárias para Trilho
+*/
+	
